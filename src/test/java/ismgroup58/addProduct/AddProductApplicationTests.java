fe2d630fbd8105eb8934e7e900ba9a4725aed116
Anastasia Lagopanagiotopoulou <147436031+AnastasiaLag@users.noreply.github.com>
2025-02-07 23:18:06 +0200

    first commit

---
+++ src/test/java/ismgroup58/addProduct/AddProductApplicationTests.java
@@ -0,0 +1,13 @@
+package ismgroup58.addProduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
