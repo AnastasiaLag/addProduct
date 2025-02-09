7241f20abe5a458a116e33a2e0f02e6a64039a01
Anastasia Lagopanagiotopoulou <147436031+AnastasiaLag@users.noreply.github.com>
2025-02-10 00:05:38 +0200

    + images, css, js

--- src/main/java/ismgroup58/addProduct/controller/LoginController.java
+++ src/main/java/ismgroup58/addProduct/controller/LoginController.java
@@ -1,5 +1,20 @@
 package ismgroup58.addProduct.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ismgroup58.addProduct.data.UserService;
+
+@Controller
 public class LoginController {
+
+    private UserService us;
     
+    @GetMapping("login")
+    public String showLoginPage() {
+        return "login.html";
+    }
+
+    
+
 }
